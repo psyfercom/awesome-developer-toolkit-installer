73a59f2a5cbe2d6b2ef5b6ec91416fb7387adf37
Christian Kessler <psyferpunk@proton.me>
2024-03-07 23:01:47 -0800

    Update install_packages.sh
    Helps organize the list with major language frameworks first, followed by libraries, and then other essential development tools and utilities.

--- install_packages.sh
+++ install_packages.sh
@@ -5,34 +5,37 @@ install_npm() {
     package=$1
     npm install -g $package
     if [ $? -eq 0 ]; then
-        echo "$package installed successfully."
+        echo "$package installed successfully with npm."
+        npm_packages+=("$package")
+        return 0
     else
-        echo "Failed to install $package with npm."
-        failed_packages+=("$package")
+        return 1
     fi
 }
 
-# Function to install packages using apt-get
-install_apt() {
+# Function to install packages using yarn
+install_yarn() {
     package=$1
-    sudo apt-get install -y $package
+    yarn global add $package
     if [ $? -eq 0 ]; then
-        echo "$package installed successfully."
+        echo "$package installed successfully with yarn."
+        yarn_packages+=("$package")
+        return 0
     else
-        echo "Failed to install $package with apt-get."
-        failed_packages+=("$package")
+        return 1
     fi
 }
 
-# Function to install packages using cargo
-install_cargo() {
+# Function to install packages using pnpm
+install_pnpm() {
     package=$1
-    cargo install $package
+    pnpm install -g $package
     if [ $? -eq 0 ]; then
-        echo "$package installed successfully."
+        echo "$package installed successfully with pnpm."
+        pnpm_packages+=("$package")
+        return 0
     else
-        echo "Failed to install $package with cargo."
-        failed_packages+=("$package")
+        return 1
     fi
 }
 
@@ -41,119 +44,150 @@ install_pip() {
     package=$1
     pip install $package
     if [ $? -eq 0 ]; then
-        echo "$package installed successfully."
+        echo "$package installed successfully with pip."
+        pip_packages+=("$package")
+        return 0
     else
-        echo "Failed to install $package with pip."
-        failed_packages+=("$package")
+        return 1
+    fi
+}
+
+# Function to install packages using cargo
+install_cargo() {
+    package=$1
+    cargo install $package
+    if [ $? -eq 0 ]; then
+        echo "$package installed successfully with cargo."
+        cargo_packages+=("$package")
+        return 0
+    else
+        return 1
     fi
 }
 
 # Initialize arrays to categorize packages
 npm_packages=()
-apt_packages=()
-cargo_packages=()
+yarn_packages=()
+pnpm_packages=()
 pip_packages=()
+cargo_packages=()
 failed_packages=()
 
 # List of packages to install
 packages_to_install=(
-    "code"
-    "git"
-    "python3"
+    # Major language frameworks
     "nodejs"
-    "docker"
-    "default-jdk"
-    "gcc"
-    "openjdk-17-jdk"
-    "postgresql"
-    "mysql-server"
-    "mongodb"
-    "openssh-client"
-    "vim"
-    "nano"
-    "sublime-text"
-    "eclipse"
-    "intellij-idea-community"
-    "android-studio"
-    "anaconda"
-    "jupyter-notebook"
-    "apache2"
-    "nginx"
-    "php"
+    "python3"
     "ruby"
-    "golang"
-    "rustc"
-    "cmake"
+    "golang-go"
+    "openjdk-11-jdk"
+    "php"
+
+    # Libraries
+    "gcc"
+    "g++"
+    "libssl-dev"
+    "libffi-dev"
+    "python3-dev"
+    "libsqlite3-dev"
+    "libxml2-dev"
+    "libxslt1-dev"
+    "libbz2-dev"
+    "libreadline-dev"
+    "libncurses5-dev"
+    "libncursesw5-dev"
+    "liblzma-dev"
+    "libcurl4-openssl-dev"
+    "libexpat1-dev"
+    "libgdal-dev"
+    "libffi-dev"
+    "libfreetype6-dev"
+    "libgeos-dev"
+    "libjpeg-dev"
+    "libmagick++-dev"
+    "libmysqlclient-dev"
+    "libpq-dev"
+    "libpng-dev"
+    "libssl-dev"
+    "libxml2-dev"
+    "libxslt-dev"
+    "zlib1g-dev"
+
+    # Other essential tools
     "make"
-    "maven"
-    "gradle"
-    "tensorflow"
-    "pytorch"
-    "python3-flask"
-    "python3-django"
-    "node-express-generator"
-    "spring"
-    "reactjs"
-    "vue"
-    "electron"
-    "qt5-default"
-    "wireshark"
-    "postman"
-    "gitlab"
-    "jenkins"
-    "travis"
-    "grafana"
-    "prometheus"
-    "elasticsearch"
-    "kibana"
+    "cmake"
+    "git"
+    "vim"
+    "emacs"
+    "nano"
+    "curl"
+    "wget"
+    "tmux"
+    "screen"
+    "ssh"
+    "openssl"
+    "zip"
+    "unzip"
+    "tar"
+    "gzip"
+    "bzip2"
+    "file"
+    "htop"
+    "iotop"
+    "iftop"
+    "nethogs"
+    "net-tools"
+    "traceroute"
+    "dnsutils"
+    "nmap"
+    "tcpdump"
+    "strace"
+    "ltrace"
+    "sysstat"
+    "procps"
+    "psmisc"
+    "lsof"
+    "jq"
+    "tree"
+    "htop"
+    "atop"
+    "iotop"
+    "iftop"
+    "docker-ce"
 )
 
-# Iterate through packages to categorize them and handle updates
+# Iterate through packages to install them using different package managers
 for package in "${packages_to_install[@]}"; do
-    # Check if package is available through npm
-    if npm_package_exists "$package"; then
-        npm_packages+=("$package")
+    if install_npm "$package"; then
         continue
     fi
 
-    # Check if package is available through apt-get
-    if apt_package_exists "$package"; then
-        apt_packages+=("$package")
+    if install_yarn "$package"; then
         continue
     fi
 
-    # Check if package is available through cargo
-    if cargo_package_exists "$package"; then
-        cargo_packages+=("$package")
+    if install_pnpm "$package"; then
         continue
     fi
 
-    # Check if package is available through pip
-    if pip_package_exists "$package"; then
-        pip_packages+=("$package")
+    if install_pip "$package"; then
         continue
     fi
 
-    # If package not found in any package manager, add to failed_packages
+    if install_cargo "$package"; then
+        continue
+    fi
+
+    echo "Failed to install $package with any package manager."
     failed_packages+=("$package")
 done
 
-# Install packages using corresponding package managers
-for package in "${npm_packages[@]}"; do
-    install_npm "$package"
-done
-
-for package in "${apt_packages[@]}"; do
-    install_apt "$package"
-done
-
-for package in "${cargo_packages[@]}"; do
-    install_cargo "$package"
-done
-
-for package in "${pip_packages[@]}"; do
-    install_pip "$package"
-done
+# Print categorized packages
+echo "Packages installed with npm: ${npm_packages[@]}"
+echo "Packages installed with yarn: ${yarn_packages[@]}"
+echo "Packages installed with pnpm: ${pnpm_packages[@]}"
+echo "Packages installed with pip: ${pip_packages[@]}"
+echo "Packages installed with cargo: ${cargo_packages[@]}"
 
 # Print failed packages if any
 if [ ${#failed_packages[@]} -gt 0 ]; then
